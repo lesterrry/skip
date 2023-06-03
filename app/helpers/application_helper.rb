3b94cfbbc929e9f89d398c0af19335ad40ff2b1a
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-06-04 00:37:36 +0300

    nice title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+# Handcrafted by Aydar N.
+# 2023
+#
+# me@aydar.media
+#
+
+# frozen_string_literal: true
+
 module ApplicationHelper
+	def explode_span(text, span_class = nil)
+		r = ''
+		open = span_class.nil? ? '<span>' : "<span class=#{span_class}>"
+		text.each_char do |i|
+			r += "#{open}#{i}</span>"
+		end
+		sanitize(r)
+	end
 end
---
+++ app/javascript/custom/gsap.js
@@ -0,0 +1,38 @@
+/*************************
+Handcrafted by Aydar N.
+2023
+
+me@aydar.media
+
+*************************/
+
+import gsap from "gsap"
+
+const $menuItems = $('nav > a')
+const $scaling = $('.scaling')
+const $spacing = $('.spacing')
+
+$menuItems.on('mouseenter', (event) => {
+	gsap.to(event.currentTarget, { width: 100, duration: 0.1 })
+})
+
+$menuItems.on('mouseleave', (event) => {
+	gsap.to(event.currentTarget, { width: 55, duration: 0.2 })
+})
+
+$scaling.on('mouseenter', (event) => {
+	gsap.to(event.currentTarget, { scale: 1.05, height: '100%', duration: 0.2 })
+})
+
+$scaling.on('mouseleave', (event) => {
+	gsap.to(event.currentTarget, { scale: 1, height: '100%', duration: 0.1 })
+})
+
+$spacing.on('mouseenter', (event) => {
+	gsap.to(event.currentTarget, { letterSpacing: '0.4em', height: '100%', duration: 0.2 })
+})
+
+$spacing.on('mouseleave', (event) => {
+	gsap.to(event.currentTarget, { scale: 1, letterSpacing: '0em', duration: 0.2 })
+})
+
