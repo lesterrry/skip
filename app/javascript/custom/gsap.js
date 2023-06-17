996896eab9de63715daedf5af4308f45493b3d5d
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-06-17 16:58:10 +0300

    bruh

--- app/javascript/custom/gsap.js
+++ app/javascript/custom/gsap.js
@@ -11,6 +11,9 @@ import gsap from "gsap"
 const $menuItems = $('nav > a')
 const $scaling = $('.scaling')
 const $spacing = $('.spacing')
+const $stretching = $('.stretching')
+
+let hoverTween;
 
 $menuItems.on('mouseenter', (event) => {
 	gsap.to(event.currentTarget, { width: 100, duration: 0.1 })
@@ -20,19 +23,29 @@ $menuItems.on('mouseleave', (event) => {
 	gsap.to(event.currentTarget, { width: 55, duration: 0.2 })
 })
 
-$scaling.on('mouseenter', (event) => {
-	gsap.to(event.currentTarget, { scale: 1.05, height: '100%', duration: 0.2 })
-})
-
-$scaling.on('mouseleave', (event) => {
-	gsap.to(event.currentTarget, { scale: 1, height: '100%', duration: 0.1 })
-})
+// $scaling.on('mouseenter', (event) => {
+// 	gsap.killTweensOf(event.currentTarget)
+// 	hoverTween = gsap.to(event.currentTarget, { scaleX: 1.05, height: '100%', duration: 0.2 })
+// })
+//
+// $scaling.on('mouseleave', (event) => {
+// 	if (hoverTween) { hoverTween.kill() }
+// 	hoverTween = gsap.to(event.currentTarget, { scaleX: 1, height: '100%', duration: 0.1 })
+// })
 
 $spacing.on('mouseenter', (event) => {
-	gsap.to(event.currentTarget, {  scaleX: 1.5, letterSpacing: event.currentTarget.classList.contains('low') ? '0.1em' : '0.4em', duration: 0.2 })
+	gsap.to(event.currentTarget, { letterSpacing: '0.1em', duration: 0.2 })
 })
 
 $spacing.on('mouseleave', (event) => {
+	gsap.to(event.currentTarget, { letterSpacing: '0em', duration: 0.2 })
+})
+
+$stretching.on('mouseenter', (event) => {
+	gsap.to(event.currentTarget, {  scaleX: 1.5, letterSpacing: '0.4em', duration: 0.2 })
+})
+
+$stretching.on('mouseleave', (event) => {
 	gsap.to(event.currentTarget, {  scaleX: 1, letterSpacing: '0em', duration: 0.2 })
 })
 
